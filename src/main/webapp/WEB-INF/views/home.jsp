a4fd464c3fe4b2274395279e17ebffe531ed6f46
yakub andi panggabean <yakub.jobs@gmail.com>
2015-11-12 09:17:51 +0700

    adding font awesome

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -6,8 +6,8 @@
   <head>
     <script data-require="lodash.js@3.5.0" data-semver="3.5.0" src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.5.0/lodash.js"></script>
     <link href="http://fonts.googleapis.com/css?family=Ubuntu:500,700" rel="stylesheet" type="text/css" />
-    <link data-require="angular-material@0.10.0" data-semver="0.10.1" rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/angular_material/0.10.1/angular-material.min.css" />
-    <link data-require="font-awesome@4.3.0" data-semver="4.3.0" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="resources/css/angular-material.min.css" />
+    <link data-require="font-awesome@4.3.0" data-semver="4.3.0" rel="stylesheet" href="resources/css/font-awesome.min.css" />
     <link rel="stylesheet" href="resources/css/style.css" />
   </head>
   <body>
@@ -19,12 +19,11 @@
     <div class="fill-height">
       <div ui-view=""></div>
     </div>
-    <script data-require="angular.js@~1.4.0" data-semver="1.4.0" src="https://code.angularjs.org/1.4.0/angular.js"></script>
-    <script data-require="angular-animate@1.4.0" data-semver="1.4.0" src="https://code.angularjs.org/1.4.0/angular-animate.js"></script>
-    <script data-require="angular-aria@1.4.0" data-semver="1.4.0" src="https://code.angularjs.org/1.4.0/angular-aria.js"></script>
-    <script data-require="angular-material@0.9.0" data-semver="0.9.4" src="https://ajax.googleapis.com/ajax/libs/angular_material/0.9.4/angular-material.min.js"></script>
-    <script data-require="ui-router@0.2.12" data-semver="0.2.15" src="//rawgit.com/angular-ui/ui-router/0.2.15/release/angular-ui-router.js"></script>
-  
+  <script  src="resources/js/angular.min.js"></script>
+  <script  src="resources/js/angular-animate.min.js"></script>
+  <script  src="resources/js/angular-aria.min.js"></script>
+  <script  src="resources/js/angular-material.min.js"></script>
+  <script  src="resources/js/angular-ui-router.js"></script>
   <!-- angular -->
   <script src="resources/apps/app.modules.js"></script>
   <script src="resources/apps/menu.service.js"></script>
--- src/main/webapp/resources/apps/menu.service.js
+++ src/main/webapp/resources/apps/menu.service.js
@@ -9,54 +9,47 @@
       function ($location) {
 
         var sections = [{
-          name: 'Home',
-          state: 'home.gettingstarted',
-          type: 'link'
-        }];
-
-        sections.push({
-          name: 'Beers',
-          type: 'toggle',
-          pages: [{
-            name: 'IPAs',
-            type: 'link',
-            state: 'home.beers.ipas',
-            icon: 'fa fa-group'
-          }, {
-            name: 'Porters',
-            state: 'home.beers.porters',
-            type: 'link',
-            icon: 'fa fa-map-marker'
-          },
-            {
-              name: 'Wheat',
-              state: 'home.beers.wheat',
+            name: 'Beers',
+            type: 'toggle',
+            icon:'fa fa-folder-o',
+            pages: [{
+              name: 'IPAs',
               type: 'link',
-              icon: 'fa fa-plus'
-            }]
-        });
-
-        sections.push({
-          name: 'Munchies',
-          type: 'toggle',
-          pages: [{
-            name: 'Cheetos',
-            type: 'link',
-            state: 'munchies.cheetos',
-            icon: 'fa fa-group'
-          }, {
-            name: 'Banana Chips',
-            state: 'munchies.bananachips',
-            type: 'link',
-            icon: 'fa fa-map-marker'
-          },
-            {
-              name: 'Donuts',
-              state: 'munchies.donuts',
+              state: 'home.beers.ipas',
+              icon: 'fa fa-file-o'
+            }, {
+              name: 'Porters',
+              state: 'home.beers.porters',
               type: 'link',
-              icon: 'fa fa-map-marker'
-            }]
-        });
+              icon: 'fa fa-file-o'
+            },
+              {
+                name: 'Wheat',
+                state: 'home.beers.wheat',
+                type: 'link',
+                icon: 'fa fa-file-o'
+              }]
+          },{
+              name: 'Munchies',
+              type: 'toggle',
+              pages: [{
+                name: 'Cheetos',
+                type: 'link',
+                state: 'munchies.cheetos',
+                icon: 'fa fa-map-marker'
+              }, {
+                name: 'Banana Chips',
+                state: 'munchies.bananachips',
+                type: 'link',
+                icon: 'fa fa-map-marker'
+              },
+                {
+                  name: 'Donuts',
+                  state: 'munchies.donuts',
+                  type: 'link',
+                  icon: 'fa fa-map-marker'
+                }]
+            }];
 
         var self;
 
