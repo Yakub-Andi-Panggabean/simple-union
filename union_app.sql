59339dd9188301767dea2b12012984eaed4f8960
yakub andi panggabean <yakub.jobs@gmail.com>
2015-12-27 22:05:43 +0700

    latest db dump

--- union_app.sql
+++ union_app.sql
@@ -33,7 +33,7 @@ CREATE TABLE `category` (
 
 /*Data for the table `category` */
 
-insert  into `category`(`category_id`,`category_name`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('6ea864cea181464296e253c89b0ea603','Clothes',1,'2015-12-16','admin','2015-12-20','admin'),('ae96da2da86b4ae680d19ed4e676a392','Food',1,'2015-12-16','admin','2015-12-17','admin'),('dad095775d0b41c993f6df3b3ccd4195','Electronic',1,'2015-12-16','admin','2015-12-20','admin');
+insert  into `category`(`category_id`,`category_name`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('6ea864cea181464296e253c89b0ea603','Clothes',1,'2015-12-16','admin','2015-12-22','admin'),('ae96da2da86b4ae680d19ed4e676a392','Food',1,'2015-12-16','admin','2015-12-22','admin'),('dad095775d0b41c993f6df3b3ccd4195','Electronic',1,'2015-12-16','admin','2015-12-22','admin');
 
 /*Table structure for table `goods` */
 
@@ -54,7 +54,7 @@ CREATE TABLE `goods` (
 
 /*Data for the table `goods` */
 
-insert  into `goods`(`goods_id`,`goods_code`,`goods_name`,`brand`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('babba2a84a59424498b8da10000b1018','TS','Test Product','yyyy',1,'2015-12-20','admin','2015-12-20','admin');
+insert  into `goods`(`goods_id`,`goods_code`,`goods_name`,`brand`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('0e7e2714b01544e0a80cda66c41f4de8','SMPK','Sempak','Tiger',1,'2015-12-20','admin','2015-12-25','admin'),('babba2a84a59424498b8da10000b1018','TS','Test Product','yyyy',1,'2015-12-20','admin','2015-12-20','admin');
 
 /*Table structure for table `goods_category` */
 
@@ -71,7 +71,7 @@ CREATE TABLE `goods_category` (
 
 /*Data for the table `goods_category` */
 
-insert  into `goods_category`(`goods_id`,`category_id`) values ('babba2a84a59424498b8da10000b1018','dad095775d0b41c993f6df3b3ccd4195');
+insert  into `goods_category`(`goods_id`,`category_id`) values ('0e7e2714b01544e0a80cda66c41f4de8','ae96da2da86b4ae680d19ed4e676a392'),('babba2a84a59424498b8da10000b1018','dad095775d0b41c993f6df3b3ccd4195');
 
 /*Table structure for table `goods_discount` */
 
@@ -81,17 +81,20 @@ CREATE TABLE `goods_discount` (
   `goods_id` varchar(60) NOT NULL,
   `start_discount` date DEFAULT NULL,
   `end_discount` date DEFAULT NULL,
-  `discount` decimal(10,0) DEFAULT NULL,
+  `discount` decimal(10,2) NOT NULL,
   `description` varchar(60) DEFAULT NULL,
   `created_date` date DEFAULT NULL,
   `created_by` varchar(60) DEFAULT NULL,
   `updated_date` date DEFAULT NULL,
   `updated_by` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`goods_id`),
+  KEY `discount` (`discount`),
   CONSTRAINT `fk_goods_discount` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`goods_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `goods_discount` */
+
+insert  into `goods_discount`(`goods_id`,`start_discount`,`end_discount`,`discount`,`description`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('babba2a84a59424498b8da10000b1018','2012-12-03','2012-12-09','0.31','diskon akhir tahun','2012-12-02','kazuya mishima','2012-12-03','jin kazama');
 
 /*Table structure for table `goods_price` */
 
@@ -110,6 +113,8 @@ CREATE TABLE `goods_price` (
 
 /*Data for the table `goods_price` */
 
+insert  into `goods_price`(`goods_id`,`price`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('0e7e2714b01544e0a80cda66c41f4de8','13000','2015-12-26','admin','2015-12-27','admin'),('babba2a84a59424498b8da10000b1018','200000','2015-12-27','admin','2015-12-27','admin');
+
 /*Table structure for table `goods_supplier` */
 
 DROP TABLE IF EXISTS `goods_supplier`;
@@ -119,13 +124,13 @@ CREATE TABLE `goods_supplier` (
   `goods_id` varchar(60) NOT NULL,
   PRIMARY KEY (`goods_id`),
   KEY `fk_supplier_goods_m_to_m` (`supplier_id`),
-  CONSTRAINT `fk_supplier_goods_m_to_m` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `fk_goods_supplier_m_to_m` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`goods_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  CONSTRAINT `fk_goods_supplier_m_to_m` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`goods_id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `fk_supplier_goods_m_to_m` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`supplier_id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `goods_supplier` */
 
-insert  into `goods_supplier`(`supplier_id`,`goods_id`) values ('6675a18f22234b12b54e42bd25c19eb0','babba2a84a59424498b8da10000b1018');
+insert  into `goods_supplier`(`supplier_id`,`goods_id`) values ('6675a18f22234b12b54e42bd25c19eb0','0e7e2714b01544e0a80cda66c41f4de8'),('6675a18f22234b12b54e42bd25c19eb0','babba2a84a59424498b8da10000b1018');
 
 /*Table structure for table `menu` */
 
@@ -146,7 +151,7 @@ CREATE TABLE `menu` (
 
 /*Data for the table `menu` */
 
-insert  into `menu`(`menu_id`,`label`,`relative_url`,`parent_id`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('055dbbf122924def8aaa81e82b40cff1','Goods','/master/goods','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('1a40147050164b1ea4f40a3c55a072fa','Authorization','/authorization',NULL,1,'2012-12-12','xxx',NULL,NULL),('1e25c49ee5cc4bf48f07662be4e0368b',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('253dd73c9ac64477a3d1818df75d113a','Order','/master/order','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('2ad65c08594d4017bbb7b9ea30c5261b','Menu','/master/menu','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('38902529a7364d1da47e88e4c8cbec87','Supplier','/master/supplier','f30476fdd840488aaa06cc72870a03b2',1,'2013-12-12','xxx',NULL,NULL),('6cdc66e2f4d94cd0b1d9a40d58598a8d','Setting Price','/price/setting_price','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('951227bbc74e4f9f91c68b8b3959b391','Discount','/transaction/discount','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('ac6a239aa35d41509aed-d76b403b2193','Category','/master/category','f30476fdd840488aaa06cc72870a03b2',1,'2013-12-12','xxx',NULL,NULL),('f30476fdd840488aaa06cc72870a03b2','Master','/master',NULL,1,'2012-12-12','xxx',NULL,NULL),('f7a57aa0ef9b4968b91f88e1494ac11b','Stock','/master/stock','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL);
+insert  into `menu`(`menu_id`,`label`,`relative_url`,`parent_id`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('055dbbf122924def8aaa81e82b40cff1','Goods','/master/goods','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('1a40147050164b1ea4f40a3c55a072fa','Authorization','/authorization',NULL,1,'2012-12-12','xxx',NULL,NULL),('1e25c49ee5cc4bf48f07662be4e0368b',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('253dd73c9ac64477a3d1818df75d113a','Order','/master/order','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('2ad65c08594d4017bbb7b9ea30c5261b','Menu','/master/menu','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('38902529a7364d1da47e88e4c8cbec87','Supplier','/master/supplier','f30476fdd840488aaa06cc72870a03b2',1,'2013-12-12','xxx',NULL,NULL),('4a541f71b1194784807e95b0d8f9c2bd',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('6cdc66e2f4d94cd0b1d9a40d58598a8d','Setting Price','/price/setting_price','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('6f5486d7767c4fa68c42c3ab089f9f6c',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('951227bbc74e4f9f91c68b8b3959b391','Discount','/transaction/discount','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL),('ac6a239aa35d41509aed-d76b403b2193','Category','/master/category','f30476fdd840488aaa06cc72870a03b2',1,'2013-12-12','xxx',NULL,NULL),('afbd728104ae45f0855f435d6216c690',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('c4689e9d5d6b4c30bc0f824a6e050216',NULL,NULL,NULL,1,'2012-12-12','xxx',NULL,NULL),('f30476fdd840488aaa06cc72870a03b2','Master','/master',NULL,1,'2012-12-12','xxx',NULL,NULL),('f7a57aa0ef9b4968b91f88e1494ac11b','Stock','/master/stock','f30476fdd840488aaa06cc72870a03b2',1,'2012-12-12','xxx',NULL,NULL);
 
 /*Table structure for table `order` */
 
@@ -219,9 +224,10 @@ DROP TABLE IF EXISTS `stock`;
 
 CREATE TABLE `stock` (
   `goods_id` varchar(60) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `max_quantity` int(11) DEFAULT NULL,
   `location` varchar(60) NOT NULL,
   `sub_location` varchar(60) NOT NULL,
-  `stock` int(11) NOT NULL,
   `created_date` date DEFAULT NULL,
   `created_by` varchar(60) DEFAULT NULL,
   `updated_date` date DEFAULT NULL,
@@ -231,6 +237,8 @@ CREATE TABLE `stock` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Data for the table `stock` */
+
+insert  into `stock`(`goods_id`,`quantity`,`max_quantity`,`location`,`sub_location`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('babba2a84a59424498b8da10000b1018',145,1200,'x','y',NULL,NULL,'2015-12-25','admin');
 
 /*Table structure for table `supplier` */
 
@@ -251,7 +259,7 @@ CREATE TABLE `supplier` (
 
 /*Data for the table `supplier` */
 
-insert  into `supplier`(`supplier_id`,`supplier_name`,`pic`,`address`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('04a0b495fc8e4e4aa77a7f06d2d43b2b','supplier name 2x','mr.yc','lonely street on the boulevard of broken dream 2-2y',0,'2015-12-18','admin','2015-12-20','admin'),('5c9fca38d19241ee9713b3718c9fa8af','supplier name 9','miss.h','wet slippy road',1,'2015-12-18','admin','2015-12-20','admin'),('6675a18f22234b12b54e42bd25c19eb0','supplier name 5','miss.h','wet slippy road',1,'2015-12-18','admin','2015-12-20','admin'),('73b304bc3f254614b804eb5c8e032caf','gg','ggg','gggg',1,'2015-12-18','admin',NULL,NULL),('7fdf8786258d468ea14acb2b654c9ba2','supplier name 4','miss.v','wet slippy',1,'2015-12-18','admin','2015-12-20','admin');
+insert  into `supplier`(`supplier_id`,`supplier_name`,`pic`,`address`,`active`,`created_date`,`created_by`,`updated_date`,`updated_by`) values ('04a0b495fc8e4e4aa77a7f06d2d43b2b','supplier name 2x','mr.yc','lonely street on the boulevard of broken dream 2-2y',1,'2015-12-18','admin','2015-12-22','admin'),('5c9fca38d19241ee9713b3718c9fa8af','supplier name 9','miss.h','wet slippy road',1,'2015-12-18','admin','2015-12-22','admin'),('6675a18f22234b12b54e42bd25c19eb0','supplier name 5','miss.h','wet slippy road',1,'2015-12-18','admin','2015-12-22','admin'),('7fdf8786258d468ea14acb2b654c9ba2','supplier name 4','miss.v','wet slippy',1,'2015-12-18','admin','2015-12-22','admin');
 
 /*Table structure for table `user` */
 
